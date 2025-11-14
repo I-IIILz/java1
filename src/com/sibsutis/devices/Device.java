90e4df9944e6a197787ff297ac988aa74a5c7a43
Дмитрий Масалитин ИП-516 <masalitin.d@gmail.com>
2025-11-14 22:52:55 +0700

    Реализован интерфейс Printable и абстрактный класс Device

--- src/com/sibsutis/devices/Device.java
+++ src/com/sibsutis/devices/Device.java
@@ -1,7 +1,43 @@
 package com.sibsutis.devices;
 
 public abstract class Device {
-    public static void main(String[] args) {
-        
+    protected int id;
+    protected int price;
+    protected String ip;
+
+    public Device(int id, int price, String ip) {
+        this.id = id;
+        this.price = price;
+        this.ip = ip;
+    }
+    public int getId(){
+        return id;
+    }
+    public int getPrice(){
+        return price;
+    }
+    public String getIp(){
+        return ip;
+    }
+
+    @Override
+    public String print() {
+        return "ID: " + id + ", Price: " + price + ", IP: " + (ip != null ? ip : "N/A");
+    }
+
+    public abstract String getDeviceType();
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Device device = (Device) obj;
+        return id == device.id &&
+                price == device.price &&
+                java.util.Objects.equals(ip, device.ip);
+    }
+    @Override
+    public int hashCode() {
+        return java.util.Objects.hash(id, price, ip);
     }
 }
